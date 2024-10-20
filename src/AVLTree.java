8a0b3f45be06e0a59389c5ebf7d6332c72927c47
christinec04 <148162581+christinec04@users.noreply.github.com>
2024-10-20 15:52:05 -0400

    Update AVLTree.java

--- src/AVLTree.java
+++ src/AVLTree.java
@@ -27,6 +27,52 @@ public class AVLTree<K> extends BinarySearchTree<K> {
             return root.isAVL();
     }
 
+    public Node left_rotate(Node n){
+        Node l = n.left; // l is right child of n
+        Node lr = l.right; // lr is right child of l
+
+        l.right = n; //
+        n.left = lr;
+
+        l.updateHeight();
+        n.updateHeight();
+
+        return l; // return new root
+    }
+
+    public Node right_rotate(Node n){
+        Node r = n.right;
+        Node rl = r.left;
+
+        r.left = n;
+        n.right = rl;
+
+        r.updateHeight();
+        n.updateHeight();
+
+        return r; // return new root
+
+    }
+
+    public Node fix(Node n) {
+        if (get_height(n.left) <= get_height(n.right)) {
+            if (get_height(n.right.left) <= get_height(n.right.right)) {
+                n = left_rotate(n);
+            }
+            else {
+                n = left_rotate(right_rotate(n));
+            }
+        }
+        else {
+            if (get_height(n.left.left) < get_height(n.left.right)) {
+                n = right_rotate(left_rotate(n));
+            }
+            else {
+                n = right_rotate(n);
+            }
+        }
+    }
+
     /**
      * TODO
      * Inserts the given key into this AVL tree such that the ordering
@@ -34,7 +80,15 @@ public class AVLTree<K> extends BinarySearchTree<K> {
      * maintained.
      */
     public Node insert(K key) {
-        return null;  // delete this line and add your code
+        // insert as BST
+        Node curr = super.insert(key);
+
+        // check if is AVL
+        if (!curr.isAVL()){
+            fix(curr);
+        }
+
+        return curr;
     }
 
     /**
@@ -44,7 +98,13 @@ public class AVLTree<K> extends BinarySearchTree<K> {
      * nothing happens.
      */
     public void remove(K key) {
-        // delete this line and add your code
+        // remove as BST
+        super.remove(key);
+
+        // check if is AVL
+        if (!this.isAVL()){
+            fix(this);
+        }
     }
 
 }
